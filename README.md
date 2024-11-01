09d4a68fa2f99e0ee1d35f793a5112f8fab4d851
Shawn/Yuxuan Tong <tongyuxuan361@gmail.com>
2024-11-01 23:40:40 +0800

    feat: lab1

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-# psp-lab1-viz-fourier-series
-Lab 1 *Visualization of Fourier Series* in course *Principles of Signal Processing* by Prof. Jia Jia at DSCT, THU
+# PSP-Lab1: Visualization of Fourier Series
+
+Lab 1 _Visualization of Fourier Series_ in course _Principles of Signal Processing_ by Prof. Jia Jia at DSCT, THU
+
+To setup the environment, please run the following command:
+
+```bash
+conda create -n viz-fs python=3.11 -y
+conda activate viz-fs
+pip install -r requirements.txt
+# pip install -r requirements-dev.txt # for development, check the comments for more details
+```
+
+Then you can run the [`exp1.ipynb`](./exp1.ipynb) or run the script:
+
+```bash
+python exp1.py
+```
+
+Finally, check the videos in the [`results`](./results) folder.
+
+# PSP-Lab1 报告: 傅⾥叶级数的可视化
+
+## 任务⼀: 可视化方波信号
+
+### 方波信号
+
+$$f(t) = 0.5 \operatorname{sgn}(\sin(t)) + 0.5$$
+
+### 方波信号的傅里叶级数
+
+计算得到 $$f(t) = 0.5 + \frac{2}{\pi} * (sint + \frac{1}{3}sin3t + ... + \frac{1}{n}sinnt)$$
+
+### 实现思路
+
+对 $n$ 分类返回对应常数即可：
+
+```python
+# For square wave, coefficients have analytical solutions
+if n == 0:
+    return 0.5  # DC component (a0)
+elif n % 2 == 0:
+    return 0  # Even coefficients are zero due to symmetry
+else:
+    # Odd coefficients follow 2/(nπ) pattern for sine terms only
+    return 2 / (math.pi * (n + 1) / 2) if n % 4 == 1 else 0
+```
+
+## 任务二: 可视化半圆波信号（选做）
+
+### 半圆波信号
+
+$$f(t) = \sqrt{\pi^2 - (t - \pi)^2}$$
+
+### 半圆波信号的傅里叶级数
+
+表达式如下：
+
+$$
+\begin{aligned}
+a_0=&\frac{1}{T_1} \int_{t_0}^{t_0+T_1} f(t) d t \\
+a_n=&\frac{2}{T_1} \int_{t_0}^{t_0+T_1} f(t) \cos (n \omega_1 t) d t \\
+b_n=&\frac{2}{T_1} \int_{t_0}^{t_0+T_1} f(t) \sin (n \omega_1 t) d t
+\end{aligned}
+$$
+
+### 实现思路
+
+难以直接求解，调用数值算法 `np.trapzoid` 积分
+
+```python
+# For semi-circle, use numerical integration
+x = np.linspace(0, 2 * math.pi, self.num_samples)  # Sample points
+y = np.zeros(self.num_samples, dtype=float)
+
+# Calculate function values at sample points
+for i in range(self.num_samples):
+    y[i] = self.semi_circle_wave(x[i])
+
+if n == 0:
+    # Calculate a0 coefficient (mean value)
+    return np.trapezoid(y, x) / (2 * math.pi)
+elif n % 2 == 0:
+    # Calculate an coefficients (cosine terms)
+    for i in range(1000):
+        y[i] = y[i] * math.cos(n / 2 * x[i])
+    return np.trapezoid(y, x) / math.pi
+else:
+    # Calculate bn coefficients (sine terms)
+    for i in range(1000):
+        y[i] = y[i] * math.sin((n + 1) / 2 * x[i])
+    return np.trapezoid(y, x) / math.pi
+```
--- exp1.py
+++ exp1.py
@@ -1,97 +1,214 @@
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Visualization of Fourier Series
+
 import math
-import numpy as np
 import os
+
 import imageio
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.pyplot import cm
-
-# TODO: 1. Change N_Fourier to 2, 4, 8, 16, 32, 64, 128, get visualization results with differnet number of Fourier Series
-N_Fourier = 64
-
-# TODO: optional, implement visualization for semi-circle
-signal_name = "square"
-
-# TODO: 2. Please implement the function that calculates the Nth fourier coefficient
-# Note that n starts from 0
-# For n = 0, return a0; n = 1, return b1; n = 2, return a1; n = 3, return b2; n = 4, return a2 ...
-# n = 2 * m - 1(m >= 1), return bm; n = 2 * m(m >= 1), return am. 
-def fourier_coefficient(n):
-    pass
-
-# TODO: 3. implement the signal function
-def square_wave(t):
-    pass
-
-# TODO: optional. implement the semi circle wave function
-def semi_circle_wave(t):
-    pass
-
-def function(t):
-    if signal_name == "square":
-        return square_wave(t)
-    elif signal_name == "semicircle":
-        return semi_circle_wave(t)
-    else:
-        raise Exception("Unknown Signal")
+from tqdm import tqdm
 
 
-def visualize():
-    if not os.path.exists(signal_name):
-        os.makedirs(signal_name)
+class FourierSeriesVisualizer:
+    """
+    Parameters
+    ----------
+    signal_name: str
+        optional, implement visualization for semi-circle
+    N_Fourier: int
+        1. Change N_Fourier to 2, 4, 8, 16, 32, 64, 128, get visualization results with differnet number of Fourier Series
+    num_samples: int
+        Number of samples for the original function.
+    """
 
-    frames = 100
+    def __init__(
+        self,
+        signal_name: str = "square",
+        N_Fourier: int = 64,
+        num_samples: int = 1000,
+    ):
+        self.signal_name = signal_name
+        self.N_Fourier = N_Fourier
+        self.num_samples = num_samples
 
-    # x and y are for drawing the original function
-    x = np.linspace(0, 2 * math.pi, 1000)
-    y = np.zeros(1000, dtype = float)
-    for i in range(1000):
-        y[i] = function(x[i])
+    def fourier_coefficient(self, n):
+        """DONE: 4. Calculate the nth Fourier coefficient for either square wave or semi-circle wave.
 
-    for i in range(frames):
-        figure, axes = plt.subplots()
-        color=iter(cm.rainbow(np.linspace(0, 1, 2 * N_Fourier + 1)))
+        For a periodic function f(t), the Fourier series is:
+        f(t) = a0/2 + Σ(an*cos(nωt) + bn*sin(nωt))
 
-        time = 2 * math.pi * i / 100
-        point_pos_array = np.zeros((2 * N_Fourier + 2, 2), dtype = float)
-        radius_array = np.zeros((2 * N_Fourier + 1), dtype = float)
+        This function returns coefficients in the following order:
+        n = 0: returns a0 (DC component)
+        n = 1: returns b1 (first sine coefficient)
+        n = 2: returns a1 (first cosine coefficient)
+        n = 3: returns b2 (second sine coefficient)
+        n = 4: returns a2 (second cosine coefficient)
+        And so on...
 
-        point_pos_array[0, :] = [0, 0]
-        radius_array[0] = fourier_coefficient(0)
-        point_pos_array[1, :] = [0, radius_array[0]]
+        For square wave:
+        - a0 = 0.5 (mean value)
+        - an = 0 (all cosine terms are zero due to odd symmetry)
+        - bn = 2/(nπ) for n odd, 0 for n even (due to half-wave symmetry)
 
-        circle = patches.Circle(point_pos_array[0], radius_array[0], fill = False, color = next(color))
-        axes.add_artist(circle)
+        For semi-circle:
+        Uses numerical integration (trapezoidal rule) to compute coefficients:
+        - a0 = (1/2π) ∫f(t)dt over [0,2π]
+        - an = (1/π) ∫f(t)cos(nt)dt over [0,2π]
+        - bn = (1/π) ∫f(t)sin(nt)dt over [0,2π]
+        """
+        if self.signal_name == "square":
+            # For square wave, coefficients have analytical solutions
+            if n == 0:
+                return 0.5  # DC component (a0)
+            elif n % 2 == 0:
+                return 0  # Even coefficients are zero due to symmetry
+            else:
+                # Odd coefficients follow 2/(nπ) pattern for sine terms only
+                return 2 / (math.pi * (n + 1) / 2) if n % 4 == 1 else 0
 
-        f_t = function(time)
-        for j in range(N_Fourier):
-            # calculate circle for a_{n}
-            radius_array[2 * j + 1] = fourier_coefficient(2 * j + 1)
-            point_pos_array[2 * j + 2] = [point_pos_array[2 * j + 1][0] + radius_array[2 * j + 1] * math.cos((j + 1) * time),   # x axis
-                                        point_pos_array[2 * j + 1][1] + radius_array[2 * j + 1] * math.sin((j + 1) * time)]     # y axis
-            circle = patches.Circle(point_pos_array[2 * j + 1], radius_array[2 * j + 1], fill = False, color = next(color))
+        elif self.signal_name == "semicircle":
+            # For semi-circle, use numerical integration
+            x = np.linspace(0, 2 * math.pi, self.num_samples)  # Sample points
+            y = np.zeros(self.num_samples, dtype=float)
+
+            # Calculate function values at sample points
+            for i in range(self.num_samples):
+                y[i] = self.semi_circle_wave(x[i])
+
+            if n == 0:
+                # Calculate a0 coefficient (mean value)
+                return np.trapezoid(y, x) / (2 * math.pi)
+            elif n % 2 == 0:
+                # Calculate an coefficients (cosine terms)
+                for i in range(1000):
+                    y[i] = y[i] * math.cos(n / 2 * x[i])
+                return np.trapezoid(y, x) / math.pi
+            else:
+                # Calculate bn coefficients (sine terms)
+                for i in range(1000):
+                    y[i] = y[i] * math.sin((n + 1) / 2 * x[i])
+                return np.trapezoid(y, x) / math.pi
+        else:
+            raise Exception("Unknown Signal")
+
+    def square_wave(self, t):
+        """DONE: 3. implement the signal function"""
+        delta_t = (t + 2 * math.pi) % (2 * math.pi)
+        y = 0 if delta_t <= 0 or delta_t >= math.pi else 1
+        return y
+
+    def semi_circle_wave(self, t):
+        """DONE: optional. implement the semi circle wave function"""
+        delta_t = (t + 2 * math.pi) % (2 * math.pi)
+        y = np.sqrt(math.pi * math.pi - (delta_t - math.pi) ** 2)
+        return y
+
+    def function(self, t):
+        if self.signal_name == "square":
+            return self.square_wave(t)
+        elif self.signal_name == "semicircle":
+            return self.semi_circle_wave(t)
+        else:
+            raise Exception("Unknown Signal")
+
+    def visualize(self, frames: int = 100, output_dir: str = "results"):
+        """
+        Parameters
+        ----------
+        frames: int
+            Number of frames for the animation
+        """
+        signal_dir = os.path.join("tmp", self.signal_name)
+        os.makedirs(signal_dir, exist_ok=True)
+
+        # x and y are for drawing the original function
+        x = np.linspace(0, 2 * math.pi, self.num_samples)
+        y = np.zeros(self.num_samples, dtype=float)
+        for i in range(self.num_samples):
+            y[i] = self.function(x[i])
+
+        for i in range(frames):
+            figure, axes = plt.subplots()
+            color = iter(cm.rainbow(np.linspace(0, 1, 2 * self.N_Fourier + 1)))
+
+            time = 2 * math.pi * i / frames
+            point_pos_array = np.zeros((2 * self.N_Fourier + 2, 2), dtype=float)
+            radius_array = np.zeros((2 * self.N_Fourier + 1), dtype=float)
+
+            point_pos_array[0, :] = [0, 0]
+            radius_array[0] = self.fourier_coefficient(0)
+            point_pos_array[1, :] = [0, radius_array[0]]
+
+            circle = patches.Circle(
+                point_pos_array[0], radius_array[0], fill=False, color=next(color)
+            )
             axes.add_artist(circle)
-            
-            # calculate circle for b_{n}
-            radius_array[2 * j + 2] = fourier_coefficient(2 * j + 2)
-            point_pos_array[2 * j + 3] = [point_pos_array[2 * j + 2][0] + radius_array[2 * j + 2] * math.sin((j + 1) * time),   # x axis
-                                        point_pos_array[2 * j + 2][1] + radius_array[2 * j + 2] * math.cos((j + 1) * time)]     # y axis
-            circle = patches.Circle(point_pos_array[2 * j + 2], radius_array[2 * j + 2], fill = False, color = next(color))
-            axes.add_artist(circle)
-            
-        plt.plot(point_pos_array[:, 0], point_pos_array[:, 1], 'o-')
-        plt.plot(x, y, '-')
-        plt.plot([time, point_pos_array[-1][0]], [f_t, point_pos_array[-1][1]], '-', color = 'r')
-        plt.gca().set_aspect('equal', adjustable='box')
-        plt.savefig(os.path.join(signal_name, "{}.png".format(i)))
-        # plt.show()
-        plt.close()
-        
-    images = []
-    for i in range(frames):
-        images.append(imageio.imread(os.path.join(signal_name, "{}.png".format(i))))
-    imageio.mimsave('{}.mp4'.format(signal_name), images)
+
+            f_t = self.function(time)
+            for j in range(self.N_Fourier):
+                # calculate circle for a_{n}
+                radius_array[2 * j + 1] = self.fourier_coefficient(2 * j + 1)
+                point_pos_array[2 * j + 2] = [
+                    point_pos_array[2 * j + 1][0]
+                    + radius_array[2 * j + 1] * math.cos((j + 1) * time),  # x axis
+                    point_pos_array[2 * j + 1][1]
+                    + radius_array[2 * j + 1] * math.sin((j + 1) * time),
+                ]  # y axis
+                circle = patches.Circle(
+                    point_pos_array[2 * j + 1],
+                    radius_array[2 * j + 1],
+                    fill=False,
+                    color=next(color),
+                )
+                axes.add_artist(circle)
+
+                # calculate circle for b_{n}
+                radius_array[2 * j + 2] = self.fourier_coefficient(2 * j + 2)
+                point_pos_array[2 * j + 3] = [
+                    point_pos_array[2 * j + 2][0]
+                    + radius_array[2 * j + 2] * math.sin((j + 1) * time),  # x axis
+                    point_pos_array[2 * j + 2][1]
+                    + radius_array[2 * j + 2] * math.cos((j + 1) * time),
+                ]  # y axis
+                circle = patches.Circle(
+                    point_pos_array[2 * j + 2],
+                    radius_array[2 * j + 2],
+                    fill=False,
+                    color=next(color),
+                )
+                axes.add_artist(circle)
+
+            plt.plot(point_pos_array[:, 0], point_pos_array[:, 1], "o-")
+            plt.plot(x, y, "-")
+            plt.plot(
+                [time, point_pos_array[-1][0]],
+                [f_t, point_pos_array[-1][1]],
+                "-",
+                color="r",
+            )
+            plt.gca().set_aspect("equal", adjustable="box")
+            plt.savefig(os.path.join(signal_dir, f"{i}.png"))
+            # plt.show()
+            plt.close()
+
+        images = []
+        for i in range(frames):
+            images.append(imageio.imread(os.path.join(signal_dir, f"{i}.png")))
+        os.makedirs(output_dir, exist_ok=True)
+        imageio.mimsave(
+            os.path.join(output_dir, f"{self.signal_name}-{self.N_Fourier}.mp4"), images
+        )
 
 
 if __name__ == "__main__":
-    visualize()
+    visualizer = FourierSeriesVisualizer()
+    for signal_name in ["square", "semicircle"]:
+        for N_Fourier in tqdm([2, 4, 8, 16, 32, 64, 128], desc=signal_name):
+            visualizer.signal_name = signal_name
+            visualizer.N_Fourier = N_Fourier
+            visualizer.visualize()
